6afd29752f72fc0d6d0f46841a3c4af4afeadd08
Chris Shannon <cshannon@mdsol.com>
2020-03-10 14:18:58 -0400

    Update test_CsvReader.py

--- Tests/test_CsvReader.py
+++ Tests/test_CsvReader.py
@@ -5,7 +5,7 @@ from CsvReader.csvReader import CsvReader, ClassFactory
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('Tests/Data/employee_birthday.csv')
+        self.csv_reader = CsvReader('Tests/csv/employee_birthday.csv')
 
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_objects('person')
