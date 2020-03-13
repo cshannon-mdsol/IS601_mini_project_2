8ebd0f5d4029fa4aab6c29fc50e5563b914d5569
Chris Shannon <cshannon@mdsol.com>
2020-03-13 18:55:47 -0400

    Making recommended PyCharm changes

--- Statistics/statistics.py
+++ Statistics/statistics.py
@@ -15,9 +15,59 @@ from Statistics.ZScore import z_score
 
 
 class Statistics(Calculator):
+    data = []
 
-    
+    def __init__(self):
+        super().__init__()
+
+    def confidence_interval_bottom(self, data):
+        self.result = confidence_interval_bottom(data)
+        return self.result
+
+    def confidence_interval_top(self, data):
+        self.result = confidence_interval_top(data)
+        return self.result
+
+    def correlation_coefficient(self, data, data1):
+        self.result = correlation(data, data1)
+        return self.result
+
+    def median(self, data):
+        self.result = median(data)
+        return self.result
 
     def mean(self, data):
         self.result = mean(data)
+        return self.result
+
+    def mode(self, data):
+        self.result = mode(data)
+        return self.result
+
+    def p_value(self, data):
+        self.result = p_value(data)
+        return self.result
+
+    def population_mean(self, data):
+        self.result = population_mean(data)
+        return self.result
+
+    def proportion(self, data):
+        self.result = proportion(data)
+        return self.result
+
+    def stddev(self, data):
+        self.result = stddev(data)
+        return self.result
+
+    def variance(self, data):
+        self.result = variance(data)
+        return self.result
+
+    def population_proportion_variance(self, data):
+        self.result = variance_of_population_proportion(data)
+        return self.result
+
+    def z_score(self, data):
+        self.result = z_score(data)
         return self.result
