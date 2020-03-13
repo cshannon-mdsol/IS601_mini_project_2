b4c0adf312f316b67b1867f03e4b75d9f5f357ba
Chris Shannon <cshannon@mdsol.com>
2020-03-13 19:01:31 -0400

    Making recommended PyCharm changes

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -1,30 +1,14 @@
 import unittest
 from CsvReader.CsvReader import CsvReader
-from Calculator.Calculator import Calculator
 from Statistics.Statistics import Statistics
 from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
-    test_data = CsvReader('Tests/Data/Test_Data.csv').data
-    column1 = [int(row['value1']) for row in test_data]
-    column2 = [int(row['value2']) for row in test_data]
-    p_answers = CsvReader('Tests/Data/Test_Proportion.csv').data
-    z_answers = CsvReader('Tests/Data/Test_ZScores.csv').data
-    column_proportion = [float(row['Proportion']) for row in p_answers]
-    column_zscore = [float(row['Z-Score']) for row in z_answers]
-    test_answer = CsvReader('Tests/Data/UnitTestStatsAnswers.csv').data
-    sample_data = CsvReader('Tests/Data/Test_Data_Sample.csv').data
-    column3 = [int(row['sample1']) for row in sample_data]
-    sample_answer = CsvReader('Tests/Data/UnitTestSampleAnswers.csv').data
-
     def setUp(self) -> None:
         self.statistics = Statistics()
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.statistics, Statistics)
-
-    def test_decorator_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
 
     def test_population_mean_statistics(self):
@@ -70,11 +54,11 @@ class MyTestCase(unittest.TestCase):
                          float(row['correlation']))
         self.assertEqual(self.statistics.result, float(row['correlation']))
 
-    def test_zscore_statistics(self):
+    def test_z_score_statistics(self):
         self.assertEqual(self.statistics.zscore(self.column1), self.column_zscore)
         self.assertEqual(self.statistics.result, self.column_zscore)
 
-    def test_pvalue_statistics(self):
+    def test_p_value_statistics(self):
         self.assertEqual(self.statistics.p_value(self.column1), self.column_zscore)
         self.assertEqual(self.statistics.result, self.column_zscore)
 
