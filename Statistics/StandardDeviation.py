e0f2ae9dd7f8f5ecfe5c45db6f2799f5e120ee5a
sderosa <C0astal89>
2020-03-17 23:27:19 -0400

    Feature: Added new file with method StandardDeviation

--- Statistics/StandardDeviation.py
+++ Statistics/StandardDeviation.py
@@ -1,11 +1,12 @@
 from Calculator.SquareRoot import square_root
 from Statistics.Variance import variance
-
+from pprint import pprint
 
 def stddev(num):
     try:
-        variance_float = variance(num)
-        return round(square_root(variance_float), 5)
+        #variance_float = variance(num)
+        pprint(num)
+        return round(square_root(num), 5)
     except ZeroDivisionError:
         print("Error - Cannot divide by 0")
     except ValueError:
