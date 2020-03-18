02bef8027751431ff598060c092f7265a302366a
sderosa <C0astal89>
2020-03-17 23:28:00 -0400

    Feature: Added new file with method Quartiles

--- Statistics/Quartiles.py
+++ Statistics/Quartiles.py
@@ -1,4 +1,17 @@
+from pprint import pprint
+
+
 def quartiles(data):
+    try:
+        # 1. find the mean of the data
+        distanceArray = []
+
+        for item in data:
+            x = sorted(distanceArray.append(item))
+            print(x)
+        for i in distanceArray:
+            x = x + square(i)
+        return division(num_values, x)
     except ZeroDivisionError:
         print("Error - Cannot divide by 0")
     except ValueError:
