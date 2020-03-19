87d686543d734e6f9f22279987311bd842a11270
sderosa <C0astal89>
2020-03-18 20:46:17 -0400

    Feature: Added SampleCorrelation and method

--- Statistics/SampleCorrelation.py
+++ Statistics/SampleCorrelation.py
@@ -1,15 +1,34 @@
 from Statistics.ZScore import z_score
+from Statistics.Mean import mean
+from Calculator.Multiplication import multiplication
+from Calculator.Subtraction import subtraction
+from Calculator.Square import square
+from Calculator.Division import division
+from Calculator.SquareRoot import square_root
+from pprint import pprint
+
 
 def sample_correlation(data, data1):
     try:
-        z1 = z_score(data)
-        z2 = z_score(data1)
-        z1_z2 = list(map(lambda x, y: x * y, z1, z2))
-        corr = sum(z1_z2) / len(z1_z2)
-        return round(corr, 7)
+        mean1 = mean(data)
+        mean2 = mean(data1)
+        pprint(float(mean1))
+        a = subtraction(data,mean1)
+        b = subtraction(data1,mean2)
+        pprint(a)
+        c = multiplication(a,b)
+        d = square(a)
+        e = square(b)
+        f=multiplication(d,e)
+        g=square_root(f)
+        h=division(c,g)
+        i=0
+
+        return h
     except ZeroDivisionError:
         print("Error - Cannot divide by 0")
     except ValueError:
         print("Error - Invalid data inputs")
 
 # https://www.statisticshowto.datasciencecentral.com/probability-and-statistics/correlation-coefficient-formula/
+# https://www.mathsisfun.com/data/correlation.html
