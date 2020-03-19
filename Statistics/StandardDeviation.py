7c47879991a69430e3a28da35ee0653f1c752969
sderosa <C0astal89>
2020-03-18 20:47:50 -0400

    Fix: Tested and debugged to get Class/function working

--- Statistics/StandardDeviation.py
+++ Statistics/StandardDeviation.py
@@ -4,9 +4,9 @@ from pprint import pprint
 
 def stddev(num):
     try:
-        #variance_float = variance(num)
-        pprint(num)
-        return round(square_root(num), 5)
+        variance_float = variance(num)
+
+        return round(square_root(variance_float), 5)
     except ZeroDivisionError:
         print("Error - Cannot divide by 0")
     except ValueError:
