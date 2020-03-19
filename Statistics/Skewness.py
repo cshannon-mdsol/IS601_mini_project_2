2ec704b942fe7748b77318c33891f70b4f659bc2
sderosa <C0astal89>
2020-03-18 20:46:01 -0400

    Feature: Added Skewness and method

--- Statistics/Skewness.py
+++ Statistics/Skewness.py
@@ -1,13 +1,32 @@
 from Statistics.Mean import mean
 from Statistics.Median import median
 from Statistics.StandardDeviation import stddev
+from pprint import pprint
 
 def skewness(data):
     try:
-        3*(mean-median)/stddev
+        distanceArray = []
+        meanDeviationValue = 0
+
+        for item in data:
+            distanceArray.append(item)
+
+            num_values = len(data)
+            x = 0
+            z= 0
+        i=0
+        for i in distanceArray:
+            x = i
+        pprint(distanceArray[1])
+        x = 3 * (distanceArray[0] - distanceArray[1]) / distanceArray[2]
+        pprint(x)
+        return x
     except ZeroDivisionError:
         print("Error - Cannot divide by 0")
     except ValueError:
         print("Error - Invalid data inputs")
 
 # https://www.statisticshowto.datasciencecentral.com/skewness/
+# https://study.com/academy/lesson/skewness-in-statistics-definition-formula-example.html
+# 3*(mean-median)/stddev
+
