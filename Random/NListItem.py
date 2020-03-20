72564bcaa3a8a03919d594cfb3bcc31aa3a7fb2a
Chris Shannon <cshannon@mdsol.com>
2020-03-20 16:53:16 -0400

    Trying to get random to work

--- Random/NListItem.py
+++ Random/NListItem.py
@@ -4,10 +4,12 @@ from numpy.random import randint
 class NListItem:
 
     @staticmethod
-    def pick_from_list(series, realm):
-        series2 = []
-        length = len(series2)
+    def pick_from_list(realm):
+        series = []
+        length = len(series)
         for each in range(realm):
             position = randint(0, length-1)
-            series2.append(series[position])
-        return series2
+            series.append(series[position])
+        return series
+
+
