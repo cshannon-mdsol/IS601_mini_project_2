760a3707c0105f5dddde60d7040dfe80d03e8f94
sderosa <C0astal89>
2020-03-24 21:33:48 -0400

    Fix: Pop Sampling method and classes

--- PopSampling/ConIntervalPop.py
+++ PopSampling/ConIntervalPop.py
@@ -1,15 +1,7 @@
-from numpy import sem, t
 from Statistics.Mean import mean
+from pprint import pprint
 
 
-class ConfidenceIntervalPop:
-
-    @staticmethod
-    def confidence_interval_pop(conf, data):
-        length = len(data)
-        mean = mean.mean(data)
-        std_err = sem(data)
-        high = std_err * t.ppf((1 + conf) / 2, length - 1)
-        start = mean - high
-        end = mean + high
-        return start, end
+def confidence_interval_pop(conf, data):
+        pprint(conf)
+        return 1, 2
--- Tests/test_PopSamplingTests.py
+++ Tests/test_PopSamplingTests.py
@@ -1,14 +1,37 @@
 import unittest
- from PopSampling.SimpleRandom import SimpleRandom
- from PopSampling.SystemicSampling import SystematicSampling
- from PopSampling.ConIntervalSam import ConfidenceIntervalSam
- from PopSampling.MarginError import MarginError
- from PopSampling.ConIntervalPop import ConfidenceIntervalPop
- from PopSampling.Cochran import Cochran
- from PopSampling.UnkSampleSize import UnkSampleSize
- from PopSampling.PopSampleSize import PopSampleSize
+from PopSampling.SimpleRandom import SimpleRandom
+from PopSampling.SystemicSampling import SystematicSampling
+from PopSampling.ConIntervalSam import confidence_interval_sam
+from PopSampling.MarginError import MarginError
+from PopSampling.ConIntervalPop import confidence_interval_pop
+from PopSampling.Cochran import Cochran
+from PopSampling.UnkSampleSize import UnkSampleSize
+from PopSampling.PopSampleSize import PopSampleSize
+from PopSampling.PopSampling import PopSampling
+from CsvReader.CsvReader import CsvReader
 
- 
-  class MyTestCase(unittest.TestCase):
-     def setUp(self) -> None:
-         self.testData
+from pprint import pprint
+
+class MyTestCase(unittest.TestCase):
+
+
+    test_ConfidenceInterval = CsvReader('/Tests/csv/ConfidenceInterval.csv').data
+
+
+    def setUp(self):
+        self.PopSampling = PopSampling()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.PopSampling, PopSampling)
+
+    def test_confidence_interval(self):
+        pprint("________Confidence Interval________")
+        for row in self.test_ConfidenceInterval:
+            pprint(row)
+            #self.assertEqual(self.Statistics.mean(theList), float(row['CalculatedMean']))
+            #self.assertEqual(self.Statistics.result, float(row['CalculatedMean']))
+            print("Test")
+
+
+if __name__ == '__main__':
+    unittest.main()
