2d22f4a9b788c2af104be541257465549e96e266
sderosa <C0astal89>
2020-03-26 21:04:41 -0400

    Feature: Added working margin of error

--- PopSampling/CIMarginError.py
+++ PopSampling/CIMarginError.py
@@ -6,16 +6,17 @@ from Calculator.Multiplication import multiplication
 from Calculator.Division import division
 from Calculator.SquareRoot import square_root
 
-def confidence_interval(n,x,s):
-    zValue = 1.96
-    n1 = square_root(n)
-    n2=division(n1,s)
-    n3=multiplication(n2,zValue)
-
-
-
-    return round(float(n3),2)
-
+def cimarginerror(n,x,s):
+    try:
+        zValue = 1.96
+        n1 = square_root(n)
+        n2=division(n1,s)
+        n3=multiplication(n2,zValue)
+        return round(float(n3),2)
+    except ZeroDivisionError:
+        print("Error: Can't Divide by 0")
+    except ValueError:
+        print ("Error: Check your data inputs")
 #https://www.dummies.com/education/math/statistics/how-to-calculate-the-margin-of-error-for-a-sample-mean/
 
     #a = 1.0 * np.array(data)
