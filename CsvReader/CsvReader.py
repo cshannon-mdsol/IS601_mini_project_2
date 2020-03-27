7f665a87248c99254a3305a5cf437c9224331237
sderosa <C0astal89>
2020-03-27 12:10:14 -0400

    Fix: Creating files to open files within Travis CI

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -12,13 +12,15 @@ class CsvReader:
     def __init__(self, filepath):
         self.data = []
         try:
+            pprint(filepath)
+            pprint(absolute_path(filepath))
             with open(absolute_path(filepath)) as text_data:
                 pprint("1")
                 csv_data = csv.DictReader(text_data, delimiter=',')
-                pprint(csv_data )
+                #pprint(csv_data )
                 for row in csv_data:
                     self.data.append(row)
-                    pprint(row)
+                    #pprint(row)
                 pprint("4")
         except OSError:
             print('cannot open', filepath)
