0e5f568eafd459ce932f3ab01330a2f340fd753b
sderosa <C0astal89>
2020-03-26 21:05:48 -0400

    Feature: Added working class PopSampling.py

--- PopSampling/PopSampling.py
+++ PopSampling/PopSampling.py
@@ -1,10 +1,11 @@
 from Calculator.Calculator import Calculator
-from PopSampling.ConfidenceInterval import confidence_interval
+from PopSampling.CIMarginError import cimarginerror
 from PopSampling.Cochran import cochran
-from PopSampling.ConfidenceIntervalTop import confidence_interval_top
-from PopSampling.ConfidenceIntervalBottom import confidence_interval_bottom
+from PopSampling.ConfidenceIntervalKnown import confidence_interval_known
+from PopSampling.ConfidenceIntervalUnknown import confidence_interval_unknown
 from PopSampling.SimpleRandom import simple_random
 from PopSampling.SystematicSampling import systematic_sampling
+from PopSampling.ConfidenceIntervals import  confidence_intervals
 
 
 class PopSampling(Calculator):
@@ -25,20 +26,26 @@ class PopSampling(Calculator):
         self.result = systematic_sampling(x, y, z)
         return self.result
 
-    def confidence_interval(self, data1, data2, data3):
-        self.result = confidence_interval(data1, data2, data3)
+    def confidence_intervals(self, data):
+        self.result = confidence_intervals(data)
         return self.result
 
-    def confidence_interval_top(self, data):
-        self.result = confidence_interval_top(data)
-        return self.result
-
-    def confidence_interval_bottom(self, data):
-        self.result = confidence_interval_bottom(data)
+    def ci_margin_error(self, data1, data2, data3):
+        self.result = cimarginerror(data1, data2, data3)
         return self.result
 
     def cochran(self, data1, data2, data3, data4):
         self.result = cochran(data1, data2, data3, data4)
         return self.result
+
+    def confidence_interval_known(self, data):
+        self.result = confidence_interval_known(data)
+        return self.result
+
+    def confidence_interval_unknown(self, data):
+        self.result = confidence_interval_unknown(data)
+        return self.result
+
+
 
     pass
