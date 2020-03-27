f632c74e62a000e97183d91de8460b3360fc09c8
sderosa <C0astal89>
2020-03-27 10:41:05 -0400

    Fix: Have travic ci read csv files

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -7,33 +7,33 @@ from pprint import pprint
 class MyTestCase(unittest.TestCase):
     global nFinal
 
-    test_mean = CsvReader('/Tests/csv/Mean.csv').data
-    test_med = CsvReader('/Tests/csv/Median.csv').data
-    test_mode = CsvReader('/Tests/csv/Mode.csv').data
-    test_var = CsvReader('/Tests/csv/Variance.csv').data
-    test_stddev = CsvReader('/Tests/csv/StdDeviation.csv').data
+    test_mean = CsvReader("/Tests/csv/Mean.csv").data
+    test_med = CsvReader("/Tests/csv/Median.csv").data
+    test_mode = CsvReader("/Tests/csv/Mode.csv").data
+    test_var = CsvReader("/Tests/csv/Variance.csv").data
+    test_stddev = CsvReader("/Tests/csv/StdDeviation.csv").data
 
-    test_quar = CsvReader('/Tests/csv/Quartiles.csv').data
-    col_quar = [row['QF'] for row in test_quar]
-    col_quar1 = [row['Value1'] for row in test_quar]
-    col_quar2 = [row['QF1'] for row in test_quar]
+    test_quar = CsvReader("/Tests/csv/Quartiles.csv").data
+    col_quar = [row["QF"] for row in test_quar]
+    col_quar1 = [row["Value1"] for row in test_quar]
+    col_quar2 = [row["QF1"] for row in test_quar]
 
-    test_skew = CsvReader('/Tests/csv/SkewnessTest1.csv').data
-    col1_skewness = [row['Value1'] for row in test_skew]
-    col10_skewness = [row['skewness'] for row in test_skew]
+    test_skew = CsvReader("/Tests/csv/SkewnessTest1.csv").data
+    col1_skewness = [row["Value1"] for row in test_skew]
+    col10_skewness = [row["skewness"] for row in test_skew]
 
-    test_sam_corr = CsvReader('/Tests/csv/Correlation.csv').data
-    col_sam_corr1 = [row['value1'] for row in test_sam_corr]
-    col_sam_corr2 = [row['value2'] for row in test_sam_corr]
-    col_sam_corr3 = [row['correlation'] for row in test_sam_corr]
+    test_sam_corr = CsvReader("/Tests/csv/Correlation.csv").data
+    col_sam_corr1 = [row["value1"] for row in test_sam_corr]
+    col_sam_corr2 = [row["value2"] for row in test_sam_corr]
+    col_sam_corr3 = [row["correlation"] for row in test_sam_corr]
 
-    test_pop_corr = CsvReader('/Tests/csv/PopulationCorrelation.csv').data
-    test_mean_dev = CsvReader('/Tests/csv/MeanDeviation.csv').data
-    test_unitTest = CsvReader('/Tests/csv/UnitTestForStatistic.csv').data
+    test_pop_corr = CsvReader("/Tests/csv/PopulationCorrelation.csv").data
+    test_mean_dev = CsvReader("/Tests/csv/MeanDeviation.csv").data
+    test_unitTest = CsvReader("/Tests/csv/UnitTestForStatistic.csv").data
 
-    test_z_score = CsvReader('/Tests/csv/ZScore.csv').data
-    col_zscore = [row['zscore'] for row in test_z_score]
-    col_zscore1 = [row['zscore2'] for row in test_z_score]
+    test_z_score = CsvReader("/Tests/csv/ZScore.csv").data
+    col_zscore = [row["zscore"] for row in test_z_score]
+    col_zscore1 = [row["zscore2"] for row in test_z_score]
 
 
 
@@ -53,9 +53,9 @@ class MyTestCase(unittest.TestCase):
                     if row[keys] != "-":
                         theList.append(float(row[keys]))
 
-            self.assertEqual(self.Statistics.mean(theList), float(row['CalculatedMean']))
-            self.assertEqual(self.Statistics.result, float(row['CalculatedMean']))
-            print(self.Statistics.result, " equals ", row['CalculatedMean'])
+            self.assertEqual(self.Statistics.mean(theList), float(row["CalculatedMean"]))
+            self.assertEqual(self.Statistics.result, float(row["CalculatedMean"]))
+            print(self.Statistics.result, " equals ", row["CalculatedMean"])
 
     def test_mean_deviation_statistics(self):
         pprint("________Mean Deviation________")
@@ -65,9 +65,9 @@ class MyTestCase(unittest.TestCase):
                 if keys != "CalculatedMean" and keys != "CalculatedMeanDeviation":
                     theList.append(float(row[keys]))
 
-            self.assertAlmostEqual(self.Statistics.meandeviation(theList), float(row['CalculatedMeanDeviation']), 5)
-            self.assertAlmostEqual(self.Statistics.result, float(row['CalculatedMeanDeviation']), 5)
-            print(self.Statistics.result, " almost equals within 5 decimal places ", row['CalculatedMeanDeviation'])
+            self.assertAlmostEqual(self.Statistics.meandeviation(theList), float(row["CalculatedMeanDeviation"]), 5)
+            self.assertAlmostEqual(self.Statistics.result, float(row["CalculatedMeanDeviation"]), 5)
+            print(self.Statistics.result, " almost equals within 5 decimal places ", row["CalculatedMeanDeviation"])
 
     def test_median_statistics(self):
         pprint("________Median________")
@@ -78,15 +78,15 @@ class MyTestCase(unittest.TestCase):
                     if row[keys] != "-":
                         theList.append(float(row[keys]))
 
-            self.assertEqual(self.Statistics.median(theList), float(row['CalculatedMedian']))
-            self.assertEqual(self.Statistics.result, float(row['CalculatedMedian']))
-            print(self.Statistics.result, " equals ", float(row['CalculatedMedian']))
+            self.assertEqual(self.Statistics.median(theList), float(row["CalculatedMedian"]))
+            self.assertEqual(self.Statistics.result, float(row["CalculatedMedian"]))
+            print(self.Statistics.result, " equals ", float(row["CalculatedMedian"]))
 
     def test_mode_statistics(self):
         pprint("________Mode________")
         for row in self.test_mode:
-            self.assertEqual(self.Statistics.mode(self.column1), float(row['mode']))
-            self.assertEqual(self.Statistics.result, float(row['mode']))
+            self.assertEqual(self.Statistics.mode(self.column1), float(row["mode"]))
+            self.assertEqual(self.Statistics.result, float(row["mode"]))
 
     def test_mode_statistics(self):
         pprint("________Median Deviation________")
@@ -98,32 +98,32 @@ class MyTestCase(unittest.TestCase):
                     if row[keys] != "-":
                         theList.append(int(row[keys]))
 
-            if row['CalculatedMode'] != "No mode found":
-                self.assertEqual(self.Statistics.mode(theList), int(row['CalculatedMode']))
-                self.assertEqual(self.Statistics.result, float(row['CalculatedMode']))
-                print(self.Statistics.result, " equals ", int(row['CalculatedMode']))
+            if row["CalculatedMode"] != "No mode found":
+                self.assertEqual(self.Statistics.mode(theList), int(row["CalculatedMode"]))
+                self.assertEqual(self.Statistics.result, float(row["CalculatedMode"]))
+                print(self.Statistics.result, " equals ", int(row["CalculatedMode"]))
             else:
-                self.assertEqual(self.Statistics.mode(theList), (row['CalculatedMode']))
-                self.assertEqual(self.Statistics.result, (row['CalculatedMode']))
-                print(self.Statistics.result, " equals ", (row['CalculatedMode']))
+                self.assertEqual(self.Statistics.mode(theList), (row["CalculatedMode"]))
+                self.assertEqual(self.Statistics.result, (row["CalculatedMode"]))
+                print(self.Statistics.result, " equals ", (row["CalculatedMode"]))
 
     def test_variance_statistics(self):
         pprint("________Variance________")
         for row in self.test_var:
-            theList = [int(row['Value1']), int(row['Value2']), int(row['Value3']), int(row['Value4']),
-                       int(row['Value5'])]
-            self.assertEqual(self.Statistics.variance(theList), float(row['Variance']))
-            self.assertEqual(self.Statistics.result, float(row['Variance']))
-            print(self.Statistics.result, " equals ", (float(row['Variance'])))
+            theList = [int(row["Value1"]), int(row["Value2"]), int(row["Value3"]), int(row["Value4"]),
+                       int(row["Value5"])]
+            self.assertEqual(self.Statistics.variance(theList), float(row["Variance"]))
+            self.assertEqual(self.Statistics.result, float(row["Variance"]))
+            print(self.Statistics.result, " equals ", (float(row["Variance"])))
 
     def test_standard_deviation_statistics(self):
         pprint("________StandardDeviation________")
         for row in self.test_var:
-            theList = [int(row['Value1']), int(row['Value2']), int(row['Value3']), int(row['Value4']),
-                       int(row['Value5'])]
-            self.assertAlmostEqual(self.Statistics.stddev(theList), float(row['stddev']))
-            self.assertAlmostEqual(self.Statistics.result, float(row['stddev']))
-            print(self.Statistics.result, " equals ", (float(row['stddev'])))
+            theList = [int(row["Value1"]), int(row["Value2"]), int(row["Value3"]), int(row["Value4"]),
+                       int(row["Value5"])]
+            self.assertAlmostEqual(self.Statistics.stddev(theList), float(row["stddev"]))
+            self.assertAlmostEqual(self.Statistics.result, float(row["stddev"]))
+            print(self.Statistics.result, " equals ", (float(row["stddev"])))
 
 
     def test_skewness_statistics(self):
@@ -169,6 +169,6 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.Statistics.result, nFinal)
         #print(self.Statistics.result, " equals ", nFinal)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 
